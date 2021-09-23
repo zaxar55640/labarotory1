26b55c34748dc6593f4295f7dd03cdc14aef9d60
zaxar55640 <90439895+zaxar55640@users.noreply.github.com>
2021-09-23 21:12:54 +0300

    project, lab1

---
+++ untitled3/cmake-build-debug/CMakeFiles/untitled3.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/untitled3.dir/main.c.obj"
+  "libuntitled3.dll.a"
+  "untitled3.exe"
+  "untitled3.exe.manifest"
+  "untitled3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/untitled3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
